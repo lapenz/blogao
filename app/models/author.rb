548da9fa9a3d3e4b47fd8d6e2add0f8442ec394f
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2017-10-26 23:01:59 -0200

    Aula dia 5/6 de outubro 2017

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
+
+  mount_uploader :avatar, AvatarUploader
   has_many :posts
+
+  attr_accessor :avatar_cache
+
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930011605) do
+ActiveRecord::Schema.define(version: 20171026234907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170930011605) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "categories", force: :cascade do |t|
